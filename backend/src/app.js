696608a18e047126f7ae99018371065b6320bf5a
Tushar Vashisht <tusharvashisht@Tushars-MacBook-Air-3.local>
2026-05-07 15:43:35 +0530

    add db connect code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,26 @@
 import express from "express";
-import prisma from "./utils/prisma";
+import prisma from "./prisma.js";
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+async function startServer() {
+  try {
+    await prisma.$connect();
+    console.log('Database connected successfully');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+}
+
+
+startServer();
